2d81317309336c0ed2c4465abe5092b09e4306d5
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-06-10 11:22:59 +0530

    feat: add better style to header

--- src/components/WrapperComponent.tsx
+++ src/components/WrapperComponent.tsx
@@ -1,11 +1,14 @@
 import { useRef } from "react";
+
 import { Header } from "./Header/Header";
+import { Main } from "./Main/Main";
 
 export const WrapperComponent = () => {
     const refs = useSectionRefs();
     return (
         <div className="h-100vh flex flex-col">
             <Header refs={refs} />
+            <Main />
         </div>
     );
 };
